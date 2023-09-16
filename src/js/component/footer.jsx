c9eeddf70fa389dc4fe38b55b066a45d1ef46f7f
omararcx <100163833+omararcx@users.noreply.github.com>
2023-09-16 02:31:22 +0000

    created landing page with react and bootstrap

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="container-fluid bg-dark text-light text-center p-4">
+            <div className="card-footer">
+                Hecho por Omar Ramirez
+            </div>
+        </div>
+    )
+}
+
+export default Footer 
